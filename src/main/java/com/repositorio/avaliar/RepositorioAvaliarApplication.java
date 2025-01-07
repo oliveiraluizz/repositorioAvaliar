98e69d4cfa021030aa9699c65836962550de6003
Luiz <oliv.luizreis@gmail.com>
2025-01-07 00:32:09 -0300

    Iniciar projeto

---
+++ src/main/java/com/repositorio/avaliar/RepositorioAvaliarApplication.java
@@ -0,0 +1,13 @@
+package com.repositorio.avaliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioAvaliarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioAvaliarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/repositorio/avaliar/RepositorioAvaliarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.repositorio.avaliar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioAvaliarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
