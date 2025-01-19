782e65787c1816fc242bf5c6fcdb850569419849
Luiz <oliv.luizreis@gmail.com>
2025-01-19 16:25:40 -0300

    Liberação do Cors

---
+++ src/main/java/com/repositorio/avaliar/CorsConfig.java
@@ -0,0 +1,21 @@
+package com.repositorio.avaliar;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // Permite todos os endpoints
+                        .allowedOrigins("http://localhost:3000") // Permite origem específica
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
+                        .allowedHeaders("*") // Permite todos os cabeçalhos
+                        .allowCredentials(true); // Permite envio de cookies
+            }
+        };
+    }
+}
